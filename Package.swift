1f2e6b5bf90b4153c12acde668b06069207eaad1
Francisco Fortes <fortesdev@gmail.com>
2023-03-09 16:16:57 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -25,6 +25,6 @@ let package = Package(
           url: "https://github.com/infobip/mobile-messaging-sdk-ios/releases/download/10.9.0/MobileMessaging.xcframework.zip",
           checksum: "0184cc0617a92d4258529609247c432d507c36b650b367dd2d883e74c029010d"),
 
-        .target(name: "MobileMessagingWrapper", dependencies: ["mobile-messaging-sdk-ios","infobip-rtc-ios"], swiftSettings: [.define("WEBRTCUI_ENABLED=1")]),
+        .target(name: "MobileMessagingWrapper", dependencies: ["MobileMessaging","infobip-rtc-ios"], swiftSettings: [.define("WEBRTCUI_ENABLED=1")]),
 	]
 )
