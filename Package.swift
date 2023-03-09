2c21fd894ef60c2d02d171c29c687980cbb09e7b
Francisco Fortes <fortesdev@gmail.com>
2023-03-09 13:58:26 +0100

    Add files via upload

---
+++ Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version: 5.7
+import PackageDescription
+
+let package = Package(
+    name: "InfobipMobileMessaging",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "InfobipMobileMessaging",
+            targets: ["MobileMessaging"]),
+        .library(
+            name: "InfobipRTC",
+            targets: ["InfobipRTC"]),
+        .library(
+            name: "WebRTC",
+            targets: ["WebRTC"])
+    ],
+    targets: [
+        .binaryTarget(
+          name: "MobileMessaging",
+          url: "https://github.com/infobip/mobile-messaging-sdk-ios/releases/download/10.9.0/MobileMessaging.xcframework.zip",
+          checksum: "0184cc0617a92d4258529609247c432d507c36b650b367dd2d883e74c029010d"),
+        .binaryTarget(
+            name: "InfobipRTC",
+            url: "https://rtc.cdn.infobip.com/ios/2/2.0.3/InfobipRTC.zip",
+            checksum: "d4bad8c82c9119ed6538fe4c091e17cfa52ffcbc25d0407ad9603ff1f7ae02c3"),
+        .binaryTarget(
+            name: "WebRTC",
+            url: "https://rtc.cdn.infobip.com/webrtc/ios/1.0.37785/WebRTC.zip",
+            checksum: "e790bc6d407cd9d3bd599d847f8e5f52f32d77d826afdc637f668cedfa544f22")
+    ]
+)
