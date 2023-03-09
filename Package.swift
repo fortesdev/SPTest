542e4e6242be6a2d1695b2950bef022dfb91117c
Francisco Fortes <fortesdev@gmail.com>
2023-03-09 14:19:02 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     ],
     products: [
         .library(
-            name: "InfobipMobileMessagingAll",
+            name: "InfobipMobileMessaging",
             targets: ["MobileMessagingWebRTCUI"]),
         .library(
             name: "InfobipMobileMessagingCore",
